507c8f4a79f1bbc8094c45f3ec4201a67f6c1118
unknown <quocbao.job106204@gmail.com>
2025-08-29 12:27:31 +0700

    fix: grafana connection

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -2,20 +2,24 @@ import { redis } from "./cache";
 import { AppDataSource } from "./database";
 import { DataSource } from "typeorm";
 import Redis from "ioredis";
+import { logger, LoggerClient } from "./logger";
 
 export class Infrastructure {
     database: DataSource;
     cache: Redis;
+    logger: LoggerClient;
 
-    constructor(dependencies: { database: DataSource; cache: Redis }) {
+    constructor(dependencies: { database: DataSource; cache: Redis; logger: LoggerClient }) {
         this.database = dependencies.database;
         this.cache = dependencies.cache;
+        this.logger = dependencies.logger;
     }
 }
 
-const infrastructure = new Infrastructure({
-    database: AppDataSource,
-    cache: redis,
-});
-
-export default infrastructure;
+export function initInfrastructure() {
+    return new Infrastructure({
+        database: AppDataSource,
+        cache: redis,
+        logger: logger,
+    });
+}
