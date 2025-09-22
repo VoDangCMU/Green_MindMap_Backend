e41f5564299a21065d0bc7e742c1293ce97290ef
hotanphong <phongho763@gmail.com>
2025-09-22 15:41:03 +0700

    feat: Create invoice APIs

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,9 +1,11 @@
 import health from "./healthController";
 import user from "./userController";
 import token from "./tokenController";
+import calories from "./caloriesController";
 
 export default {
     health,
     user,
-    token
+    token,
+    calories
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import checkRoutes from "@root/routes/checkRoutes";
 import userRoutes from "@root/routes/userRoutes";
 import tokenRouter from "@root/routes/tokenRoutes";
+import caloriesRouter from "@root/routes/caloriesRoutes";
 const router = Router();
 
 router.use(checkRoutes);
 router.use(userRoutes);
 router.use(tokenRouter);
+router.use(caloriesRouter);
 
 export default router;
